88bae50c867eb14b868bff1587d07f1567b45898
aidris823 <pokeanand@gmail.com>
2018-11-14 11:02:33 -0500

    Parse_args - while loop to parse through argument.

---
+++ arguments.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+char ** parse_args(char * line){
+  char * s;
+  s = strsep(line," ");
+
+  char steve[5];
+  
+  while (
+}
+int main(){
+  return 0;
+}
