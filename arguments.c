28da241c59566a5667c8d55246bc14623d9b4bd4
Meredith <mhu2@stuy.edu>
2018-11-16 09:59:27 -0500

    finally fixed

--- arguments.c
+++ arguments.c
@@ -1,4 +1,3 @@
-
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -8,35 +7,19 @@
 char ** parse_args(char * line){
   char ** steve = (char **)malloc(5*sizeof(char*));
   int counter = 0;
-  
   char *s;
-  while(line != NULL){
-    printf("%s\n",line);
-    //printf("%s\n",s);
-    s = strsep(&line,"-");
-    printf("s after strsep: %s\n",s);
-    printf("line after strsep: %s\n",line);
+  while(line){
+    s = strsep(&line," ");
     steve[counter] = s;
-    printf("what's at steve[counter] now: %s\n",steve[counter]);
-    //strcpy(s,steve[counter]);
-    //line = s;
     counter++;
   }
-  printf("reached\n");
-  steve[counter+1] = NULL;
-  printf("what's steve[counter]: %s\n",steve[counter]);
   return steve;
 }
-int main(){
-  char l[100] = "woah-this-is-cool";
-  char *s1 = l;
 
-  char * s2;
-  char ** a = parse_args(s1);
-  while(a != NULL){
-    printf("%s\n",*a);
-    a++;
-  }
+int main(){
+  char line[100] = "ls -a -l";
+  char ** args = parse_args(line);
+  execvp(args[0], args);
   return 0;
 }
 
